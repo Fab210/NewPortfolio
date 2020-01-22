ee01c654a4479087e99d8d2ce363ad30f05d4c60
fabio <fabio@W-FASA-01>
2020-01-22 15:46:26 +0100

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PortfolioPageComponent } from './portfolio-page/portfolio-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: 'portfolio', component: PortfolioPageComponent },
+  { path: '', redirectTo: '/portfolio', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
