2d2a08e4c4c6054d5339cbb0bb9f5a74fbb8784c
fabio <fabio@W-FASA-01>
2020-03-16 17:38:37 +0100

    mobile responsive style optimisation

--- src/app/pipe/workDate.ts
+++ src/app/pipe/workDate.ts
@@ -2,18 +2,17 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({
   name: 'workDate'
 })
+
+//**-- check witch Work Is The Present Work --**//
+
 export class WorkDatePipe implements PipeTransform {
-  
   transform(items: Array<any>): Array<any> {
-    
-   
     let i
     for (i = 0; i < items.length; i++) {
       if (items[i].isCurrentRole == true) {
         items[i].endDate = 'Present'
       }
     };
-    
     
     return items;
   }
