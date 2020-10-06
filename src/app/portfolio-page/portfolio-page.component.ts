03a9c05d2bd585358e93327040b4231f98db8a53
Your Name <you@example.com>
2020-10-06 22:38:04 +0200

    correction path pdf for github page

--- src/app/portfolio-page/portfolio-page.component.ts
+++ src/app/portfolio-page/portfolio-page.component.ts
@@ -48,7 +48,7 @@ export class PortfolioPageComponent implements OnInit {
   }
 
   downloadPdf() {
-    const pdfUrl = './assets/CVdevsite3.pdf';
+    const pdfUrl = 'github.com/Fab210/NewPortfolio/raw/master/docs/assets/CVdevsite3.pdf';
     const pdfName = 'CV_de_Fabio';
     FileSaver.saveAs(pdfUrl, pdfName);
   }
