17c7e4ec0a7189ad75a3d3fa78754f60f85a6c20
Fabio Santos <Fabio.Santos@tns-ilres.com>
2021-08-26 16:29:12 +0200

    update design

--- src/app/portfolio-page/portfolio-page.component.ts
+++ src/app/portfolio-page/portfolio-page.component.ts
@@ -11,48 +11,41 @@ const FileSaver = require('file-saver');
   selector: "app-portfolio-page",
   templateUrl: "./portfolio-page.component.html",
   styleUrls: ["./portfolio-page.component.css"],
-  
+
 })
 export class PortfolioPageComponent implements OnInit {
   public profileDataList: any = [];
   public isloading = false;
   public animate = false;
   public routeTransitionAnimations = trigger('triggerName', []);
-  
-  constructor(private profileDataService: ProfileDataService) {}
+
+  constructor(private profileDataService: ProfileDataService) { }
 
   ngOnInit() {
-      this.getProfile();
+    this.getProfile();
   }
 
-   getProfile() {
-     this.profileDataService.getProfileData().subscribe((res:any) => {
+  getProfile() {
+    this.profileDataService.getProfileData().subscribe((res: any) => {
       console.log(res);
-       this.profileDataList = res;
-       this.isloading = true;
+      this.profileDataList = res;
+      this.isloading = true;
     });
-   }
+  }
 
 
   addAnimationInContainer() {
-   
     let element = document.getElementById("animationContainer");
-    element.classList.remove("scale-in-hor-center");
+    element.classList.remove("fade-in-left");
     void element.offsetWidth; // trigger a DOM reflow
-    element.classList.add("scale-in-hor-center");
-    
+    element.classList.add("fade-in-left");
   }
 
-  goLinkedIn(){
-    
+  goLinkedIn() {
     window.open('https://www.linkedin.com/in/fabio-santos88', "_blank");
   }
 
   downloadPdf() {
     window.open('https://github.com/Fab210/NewPortfolio/raw/master/docs/assets/CV_Fabio_Santos.pdf', "_blank");
-    //const pdfUrl = 'github.com/Fab210/NewPortfolio/raw/master/docs/assets/CVdevsite3.pdf';
-    //const pdfName = 'CV_de_Fabio';
-    //FileSaver.saveAs(pdfUrl, pdfName);
   }
-
 }
